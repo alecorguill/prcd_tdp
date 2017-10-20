488405197ec333246dd20f7224bbf7b29f000fac
alecorguill <arthur.lecorguille@gmail.com>
2017-10-20 15:44:08 +0200

    util.c

--- td1/header/util.h
+++ td1/header/util.h
@@ -6,6 +6,4 @@ double * alloue_matrice(int m, int n)
 double * alloue_vecteur(int m);
 void init_matrice(double* a, int m, int n, int lda, double value);
 void init_vecteur(double* a, int m, double value);
-void free_matrice(int m, int n);
-void free_vecteur(int m);
 #endif

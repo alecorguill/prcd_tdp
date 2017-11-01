58c98065e434091fea98da3377f0b44225998be9
ahcene <amahtout@enseirb-matmeca.fr>
2017-11-01 13:19:16 +0100

    modification analyse python

--- td1/test/analyse_perf.py
+++ td1/test/analyse_perf.py
@@ -1,17 +1,26 @@
+import csv
 import matplotlib.pyplot as plt
-sizes, flops = [], []
-with open('flop_vecteur.csv') as fp:
-    for line in fp:
-        tmp = line.split(',')
-        if tmp[0] == 'size':
-            continue
-        sizes += [int(tmp[0])]
-        flops += [float(tmp[1])]
 
-# Plotting 
-fig, ax = plt.subplots()
-ax.plot(sizes, flops)
-ax.set(xlabel='Size of the matrix (N)', ylabel='Flop per second (flops)',
-       title='Relationship between Flops and size of a given matrix')
-fig.savefig("flops_and_sizes")
-plt.show()
+def plot_csv(filename, save=False):
+    # Extracting
+    with open(filename) as csvfile:
+        rows = list(csv.reader(csvfile))
+    features = rows.pop(0) 
+    sizes = map(lambda x:int(x[0]), rows)
+    rows = map(lambda x:x[1:], rows)
+
+    # Plotting
+    name = filename.split('.')[0]
+    for ind_feature in range(0,len(features)-1):
+
+        feature = features[ind_feature+1]
+        fig, ax = plt.subplots()
+        flops = map(lambda x:float(x[ind_feature]), rows)
+        
+        ax.plot(sizes, flops)
+        ax.set(xlabel='Size of the matrix (N)', ylabel='Flop per second (flops)',
+               title= name + feature)
+        if save:
+            fig.savefig(name + '_' + ''.join(feature.split()));
+
+plot_csv('flop_matrice.csv', True)

c3f6219ca35a4834550cce1a1e9c3d4cb696cdc7
ahcene <amahtout@toctoc.pedago.ipb.fr>
2017-11-02 19:55:24 +0100

    Xmatrice de taille quelconque

--- td1/src/blas.c
+++ td1/src/blas.c
@@ -4,8 +4,8 @@
 
 #include "util.h"
 #include "blas.h"
-
-
+#define NUM_THREADS 5
+#define TB 2
 
 void dgemm_parallel(const enum CBLAS_ORDER Order, const enum CBLAS_TRANSPOSE TransA,
 	   const enum CBLAS_TRANSPOSE TransB, const int M, const int N, 
@@ -17,60 +17,67 @@ void dgemm_parallel(const enum CBLAS_ORDER Order, const enum CBLAS_TRANSPOSE Tra
   int MY_LIB_NUM_THREADS=1;
   if(var != NULL)
     MY_LIB_NUM_THREADS=atoi(var);
-    init_matrice(C,M,M,ldc,0.0);
-  if(TransA != CblasTrans){
-    //Transposer la matrice
-  }
-  else {
-    // nombre de blocs
-    int tb = 3;
-    int nb = M/tb;
-    for (int i = 0; i < nb; i++){
-#pragma omp parallel for
-      for (int j = 0; j < nb; j++){
-	double* tmp = malloc(sizeof(double)*tb*tb);
-	init_matrice(tmp,tb,tb,tb,0.0);
-	for (int k =0; k < nb; k++){
-	  cblas_dgemm_scalaire(tb, A+i*lda*tb+k*tb, M, B+j*ldb*tb+k*tb, M, tmp,tb);
-	  somme_matrice(tb,tb,C+j*ldc*tb+i*tb, ldc, tmp, tb, C+j*ldc*tb+i*tb, ldc);
-	}
-	// cas du reste
-	free(tmp);
-      }
-    }
-  }
-  
-}
-
-
-void dgemm(const enum CBLAS_ORDER Order, const enum CBLAS_TRANSPOSE TransA,
-	   const enum CBLAS_TRANSPOSE TransB, const int M, const int N, 
-	   const int K, const double alpha,
-	   const double *A, const int lda, const double *B, const int ldb,
-	   const double beta, double *C, const int ldc)
-{
+  else
+    MY_LIB_NUM_THREADS = NUM_THREADS;
+  omp_set_num_threads(MY_LIB_NUM_THREADS);
+  //
   init_matrice(C,M,M,ldc,0.0);
-  if(TransA != CblasTrans){
-    //Transposer la matrice
-  }
-  else {
-    // taille des blocs 
-    int tb = 2;
-    // nombre de blocs
-    int nb = M/tb;
-    for (int i = 0; i < nb; i++){
-      for (int j = 0; j < nb; j++){
-	double* tmp = malloc(sizeof(double)*tb*tb);
-	init_matrice(tmp,tb,tb,tb,0.0);
-	for (int k =0; k < nb; k++){
-	  cblas_dgemm_scalaire(tb, A+i*lda*tb+k*tb, M, B+j*ldb*tb+k*tb, M, tmp,tb);
-	  somme_matrice(tb,tb,C+j*ldc*tb+i*tb, ldc, tmp, tb, C+j*ldc*tb+i*tb, ldc);
-	}
+  int nb_l = M/TB;
+  int nb_c = N/TB;
+  int n,p; double* tmp;
+    
+    for (int i = 0; i < nb_l; i++){
+#pragma omp parallel for nowait
+    for (int j = 0; j < nb_c; j++){
+      
+      p = (j == nb_c-1) ? j%TB : TB;
+      
+      for (int k =0; k < nb_l; k++){
+	n = (k == nb_l-1) ? k%TB : TB;
+	tmp = malloc(sizeof(double)*n*p);
+	cblas_dgemm_scalaire(n,A+i*lda*TB+k*TB, M, B+j*ldb*TB+k*TB, M, tmp,n);
+	
+	printf("i, j, k  : %d, %d, %d / %d-%d\n", i,j,k,n,p);
+	affiche(n,p,tmp,n,1);
+	somme_matrice(n,p,C+j*ldc*TB+i*TB, ldc, tmp, TB, C+j*ldc*TB+i*TB, ldc);
 	free(tmp);
-      }
+	
+	}
+      // cas du reste
+
     }
   }
 }
+
+
+/* void dgemm(const enum CBLAS_ORDER Order, const enum CBLAS_TRANSPOSE TransA, */
+/* 	   const enum CBLAS_TRANSPOSE TransB, const int M, const int N,  */
+/* 	   const int K, const double alpha, */
+/* 	   const double *A, const int lda, const double *B, const int ldb, */
+/* 	   const double beta, double *C, const int ldc) */
+/* { */
+/*   init_matrice(C,M,M,ldc,0.0); */
+/*   if(TransA != CblasTrans){ */
+/*     //Transposer la matrice */
+/*   } */
+/*   else { */
+/*     // taille des blocs  */
+/*     int tb = 2; */
+/*     // nombre de blocs */
+/*     int nb = M/tb; */
+/*     for (int i = 0; i < nb; i++){ */
+/*       for (int j = 0; j < nb; j++){ */
+/* 	double* tmp = malloc(sizeof(double)*tb*tb); */
+/* 	init_matrice(tmp,tb,tb,tb,0.0); */
+/* 	for (int k =0; k < nb; k++){ */
+/* 	  cblas_dgemm_scalaire(tb, A+i*lda*tb+k*tb, M, B+j*ldb*tb+k*tb, M, tmp,tb); */
+/* 	  somme_matrice(tb,tb,C+j*ldc*tb+i*tb, ldc, tmp, tb, C+j*ldc*tb+i*tb, ldc); */
+/* 	} */
+/* 	free(tmp); */
+/*       } */
+/*     } */
+/*   } */
+/* } */
 
 
 /* cblas_dgemm_scalaire_ijk non carré */

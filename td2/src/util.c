f917fef01068df54f3cb333cf4640e172ffd1dde
alecorguill <arthur.lecorguille@gmail.com>
2017-11-03 17:22:02 +0100

    d

--- td2/src/util.c
+++ td2/src/util.c
@@ -1,6 +1,8 @@
 #include <util.h>
 #include <particule.h>
 
+#define TAILLE_LIGNE 81
+
 double nouveau_dt(particule* univers, int m){
   int n = 0;
   double d_min = 0.0;
@@ -15,3 +17,17 @@ double nouveau_dt(particule* univers, int m){
 }
 
 double solution_equ(particule);
+
+void parse_particules(char *filename, particule *ps){
+  FILE *f = NULL;
+  char ligne[TAILLE_LIGNE];
+  f=fopen(filename, "r");
+  fclose(f);
+  int nb_ligne=0;
+  fgets(ligne, TAILLE_LIGNE, fichier);
+  int nb_particule = ligne
+  while ( fgets(ligne, TAILLE_LIGNE, fichier) != NULL ){
+    nb_ligne
+  }
+    printf("%s", ligne ) ;
+}

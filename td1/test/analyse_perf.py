69e6c04f0de4b82fab7fdec2d14ffdcbbab38be9
alecorguill <arthur.lecorguille@gmail.com>
2017-11-03 13:59:55 +0100

    td2

--- td1/test/analyse_perf.py
+++ td1/test/analyse_perf.py
@@ -6,28 +6,31 @@ def plot_csv(filename, save=False):
     # Extracting
     with open(filename) as csvfile:
         rows = list(csv.reader(csvfile))
-    features = rows.pop(0) 
+    features = rows.pop(0)
+    features = features[:2]
     sizes = map(lambda x:int(x[0]), rows)
     rows = map(lambda x:x[1:], rows)
 
     # Plotting
     name = filename.split('.')[0]
+    fig, ax = plt.subplots()
+       
     for ind_feature in range(0,len(features)-1):
 
         feature = features[ind_feature+1]
-        fig, ax = plt.subplots()
         flops = map(lambda x:float(x[ind_feature]), rows)
         
-        ax.plot(sizes, flops)
-        ax.set(xlabel='Size of the matrix (N)', ylabel='Flop per second (flops)',
-               title= name + ' ' + feature)
+        ax.plot(sizes, flops,label=feature)
+        ax.set(xlabel='Size of the matrix (N)', ylabel='MFlop per second (Mflop/s)',
+               title= name)
         axes = plt.gca()
-        axes.set_xlim([3000,sizes[-1]])
-        if save:
-            fig.savefig(name + '_' + ''.join(feature.split()));
+        axes.set_xlim([0,sizes[-1]])
+    if save:
+        fig.savefig(name + '_' + ''.join(feature.split()));
 
 if __name__ == '__main__':
     args = sys.argv
     for i in range(1, len(args)):
-        plot_csv(args[i], save=True)
-    #plt.show()
+        plot_csv(args[i])
+    #plt.legend()
+    plt.show()

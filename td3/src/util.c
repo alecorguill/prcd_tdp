3861967adef67ed048e8424b394a7b0e20c2644a
ahcene <amahtout@skicross.pedago.ipb.fr>
2017-11-17 17:39:06 +0100

    fichier utile

---
+++ td3/src/util.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <util.h>
+#include <string.h>
+#include <unistd.h>
+
+double* parse_matrix(char* file, double *M){
+  FILE* f = fopen(file,"r");
+  if (f == NULL){
+    perror("fopen: cannot open the file\n");
+    return EXIT_FAILURE;
+  }
+  char line[MAX_LINE];
+  int N = atoi(fgets(line,MAX_LINE,fd));
+  int i = 0;
+  char *token; char delim[] = " ";
+  while(fgets(line,MAX_LINE,fd)){
+    token = strtok(line,delim);
+    for (int j = 0; j < N; j++){
+       token = strtok(NULL,delim);
+       *(M+j+i*N) = atof(token);
+    }
+    i++;
+  }
+  return M;
+}
+
+void print_matrix(double* A, int N, int fd){
+  for (int i = 0; i < N; i++){
+    for (int j = 0; j < N; j++)
+      dprintf(fd, "%f ", A[j*N+i]);
+    dprintf(fd, "\n");
+ }
+}
+
+
+

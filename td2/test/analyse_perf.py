2f258ef635c8c199309df5df8cf3405be7bef720
Arthur LE CORGUILLE <prcd2017-lecorguille@mistral01.formation.cluster>
2017-11-19 17:12:13 +0100

    f

--- td2/test/analyse_perf.py
+++ td2/test/analyse_perf.py
@@ -46,10 +46,10 @@ def perf_par(input, output, save_to_csv=False):
     time_par = []
     os.system("make -C ../ clean")
     os.system("make -C ../ parallel")
-    N = range(np_default,nb_part_default,np_default) 
-    
+    N = []
+    n = 2
     # Perf parallel
-    for n in N:
+    while n < nb_part_default:
         ##
         os.system("python generate_particules.py {} {}".format(input,n))
         #print("mpirun -np {} ./parallel {} {}".format(np_default,input,output))
@@ -57,6 +57,11 @@ def perf_par(input, output, save_to_csv=False):
         os.system("mpirun -np {} ./parallel {} {}".format(np_default,input,output))
         end = time.time()
         time_par.append(end-start)
+        N.append(n)
+        n = int(n*1.5+1)
+        n = n+1 if n%2 == 1 else n
+        print n
+        
     if save_to_csv:
         save_csv('perf_par.csv',N,time_par,'size','perf_par')
     return time_par
@@ -105,8 +110,8 @@ def speed_up_f(speed_par, input, output, save_to_csv=False):
 
 def perf(input, output):
     time_seq, time_par, speed_seq, speed_par, speed_up = [],[],[],[],[] 
-    time_seq = perf_seq(input, output,True)
-    #time_par = perf_par(input, output,True)
+    #time_seq = perf_seq(input, output,True)
+    time_par = perf_par(input, output,True)
     # speed_par = speed_par_f(input, output,True)
     # speed_up = speed_up_f(speed_par,input, output, True)
     return time_seq,time_par,speed_par,speed_up

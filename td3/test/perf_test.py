55be780abec556d5d82e88cc373b7f2f171169b0
Arthur LE CORGUILLE <prcd2017-lecorguille@mistral01.formation.cluster>
2017-12-04 16:22:47 +0100

    perf.py

--- td3/test/perf_test.py
+++ td3/test/perf_test.py
@@ -14,21 +14,24 @@ def gemm_fox_N(Np, N_lim):
     filename_B = " matrix_B.txt"
     filename_C = " matrix_C.txt"
     measures = []
-    for n in range(N_lim):
-        N = n**2
+    N = np
+    while N < N_lim:
         os.system("python generate_matrix.py" + filename_A + " " + str(N))
         os.system("python generate_matrix.py" + filename_B + " " + str(N))
         time = float(os.popen("mpirun -n " + str(Np) + " ../gemm_fox" + filename_A + filename_B + filename_C).read())
         measures.append([n,time])
+        N = N*2
     return measures
 
 
+# N has to be disivisble by all squared numbers (2²,3²,4²...,Np_lim²)
 def gemm_fox_Np(N, Np_lim):
     filename_A = " matrix_A.txt"
     filename_B = " matrix_B.txt"
     filename_C = " matrix_C.txt"
     measures = []
-    for Np in range(1,Np_lim,2):
+    l = [x*x for x in range(Np_lim)]
+    for Np in l:
         os.system("python generate_matrix.py" + filename_A + " " + str(N))
         os.system("python generate_matrix.py" + filename_B + " " + str(N))
         time = float(os.popen("mpirun -n " + str(Np) + " ../gemm_fox" + filename_A + filename_B + filename_C).read())

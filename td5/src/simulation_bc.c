560eeef71245db1bd869161892aaefc210f6bea3
ahcene <mahtout.ahcene@gmail.com>
2017-12-15 16:09:40 +0100

    fichier simulation_bc

---
+++ td5/src/simulation_bc.c
@@ -0,0 +1,94 @@
+#include <simulation_seq.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <particule.h>
+#include <util.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define NB_ITERATIONS 20
+#define THETA 1e-6
+
+int main(int argc, char** argv){
+	int size; char ligne[MAX];
+	if (argc != 4) {
+		printf("Erreur : Argument manquant\n");
+		printf("Usage : ./exec particules_file nb_blocs output_file\n");
+		return EXIT_FAILURE;
+	}
+	/* Fichier de particules */
+	FILE* fd =fopen(argv[1], "r");
+	if (!fd){
+		perror("Erreur ouverture fichier\n");
+		return EXIT_FAILURE;
+	} 
+	/* Fichier d'output des résultats */
+	int output = open(argv[3], O_CREAT | O_RDWR, 0755);
+	if (output == -1){
+		perror("open : fichier output\n");
+		return EXIT_FAILURE;
+	} 
+
+       	fgets(ligne, MAX, fd);
+	size = atoi(ligne);
+	fclose(fd);
+	fflush(stdout);
+	
+	int k;
+	int i = 0, n = 0, p = 0;
+	double dt = 0.0, t = 0.0; 
+
+	/* Recuperation des particules à partir du fichier */
+	int nb_blocs = atoi(argv[2]);
+	int dims[nb_blocs];	
+	particule univers[size];
+	parse_particules_bloc(argv[1], univers, dims);
+	
+	/* Calcul des centres de masses */
+	particules boxes_center[nb_blocs];
+	int beg = 0, end = dims[0];
+	for (int b = 0; b < nb_bloc; b++){
+	  masse_center(univers,0,beg,end,boxes_center[b]);
+	  beg += dims[b];
+	  end += dims[b];
+	}
+
+	vecteur force_tmp;
+	while (i < NB_ITERATIONS){
+		// calcul des forces exterieures
+		while (n < m){
+			while (p < m){
+				if (n == p){
+					p++;
+					continue;
+				}
+				// calcul de la distance de la particule la plus proche (pour dt)
+				double dist = distance(&univers[p], &univers[n]);
+				if (univers[n].proche_d == 0.0 ||
+						dist < univers[n].proche_d){
+					univers[n].proche_d = dist;
+				}
+				force_grav(&univers[p], &univers[n], &force_tmp);
+				somme(&(univers[n].f_ext),&(force_tmp),&(univers[n].f_ext));
+				p++;
+			}
+			p=0;
+			n++;
+		}
+		n = 0;
+		dt = nouveau_dt(univers, m);
+		t += dt;
+		/* log de dt */
+		dprintf(output, "%lf\n", t);
+		/* mise à jour des particules */
+		update_particules(univers, m, dt);
+		/* log des particules */
+		log_particules(univers, m, output);
+		i++;
+	}
+	close(output);
+	return 0;
+}

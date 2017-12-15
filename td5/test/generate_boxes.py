7a09b713a3c2cc311d5bbb2737cebc452f915a7c
alecorguill <alecorguill@laposte.net>
2017-12-15 15:14:33 +0100

    bloc generate

--- td5/test/generate_boxes.py
+++ td5/test/generate_boxes.py
@@ -8,7 +8,7 @@ def generate_boxes(filename, n1, n2):
     fd = open(filename, "w")
     n = n1+n2
     small_size = 0.1*P_RANGE
-    fd.write(str(n)+'\n')
+    fd.write(str(n1)+'\n')
     for i in range(n1):        
         masse = random.randrange(1,5000)
         px = random.uniform(0,small_size)
@@ -16,7 +16,7 @@ def generate_boxes(filename, n1, n2):
         vx = random.uniform(-V_RANGE,V_RANGE)
         vy = random.uniform(-V_RANGE,V_RANGE)
         fd.write(("%s %s %s %s %s\n")% (masse,px,py,vx,vy))
-
+    fd.write(str(n2)+'\n')
     for i in range(n2):        
         masse = random.randrange(1,5000)
         px = random.uniform(P_RANGE-small_size,P_RANGE)
@@ -29,6 +29,6 @@ def generate_boxes(filename, n1, n2):
 
 if __name__ == "__main__":
     args = sys.argv
-    if len(args) < 3:
+    if len(args) < 4:
         print "USAGE : python generate_particules.py filename n1 n2"
     generate_boxes(args[1],int(args[2]), int(args[3]))                      

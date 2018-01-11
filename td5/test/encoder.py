310a0356e9587aa154f5619fc3baaea4877f222c
ahcene <mahtout.ahcene@gmail.com>
2018-01-11 17:54:07 +0100

    TP5 - Encoder

---
+++ td5/test/encoder.py
@@ -0,0 +1,19 @@
+import random
+import sys
+
+def encoder(input,output):
+    nb_boxes = 0
+    with open(input) as fi:
+        with open(output,'w') as fo:
+            fo.write(fi.readline())
+            for line in fi:
+                if (len(line.split(' ')) != 1):
+                    fo.write(line)
+                
+if __name__ == "__main__":
+        args = sys.argv
+        if len(args) != 3:
+            print "USAGE : python encoder.py input output"
+        input = args[1]
+        output = args[2]
+        encoder(input,output)

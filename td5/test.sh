0b5c075900bfeb85e409bc623abe44f2f862b7e9
arthur <arthur@localhost.localdomain>
2018-01-14 15:16:05 +0100

    script speed up

--- td5/test.sh
+++ td5/test.sh
@@ -1,15 +1,27 @@
-# rm test/*.txt
-# python test/generate_boxes.py test/particules_bc.txt 2 500
-# python test/encoder.py test/particules_bc.txt test/particules.txt
+#!/bin/bash
+
+
+N_MAX=10000
+RANGE=100
+
+python test/generate_particules.py test/particules.txt $N_MAX
 make clean
 make sequentiel
-make sequentiel_bc
+echo "################# Sequentiel non bloqué ###############"
 time ./sequentiel test/particules.txt test/output_seq.txt
-time ./sequentiel_bc test/particules_bc.txt 4 100 test/output_bc_seq.txt
 
-# make clean
-# make parallel_bc
-# time mpirun -np 4 --mca pml ob1 ./parallel_bc test/particules_bc.txt 100 test/output_bc.txt
+for i in {1..5..1}
+do
+    np=$(($i*$i))
+    echo "############## $np processus/bloc ################"
+    ppb=$((N_MAX/$np))
+    make clean
+    make sequentiel_bc
+    python test/generate_boxes.py test/particules_bc.txt $i $ppb
+    time ./sequentiel_bc test/particules_bc.txt $np $RANGE test/output_bc_seq.txt
 
-python test/tests.py test/output_bc_seq.txt test/output_seq.txt 
-# python test/tests.py test/output_bc.txt test/output_seq.txt 
+    make clean
+    make parallel_bc
+    time mpirun -np $np --mca pml ob1 ./parallel_bc test/particules_bc.txt $RANGE test/output_bc.txt
+
+done
